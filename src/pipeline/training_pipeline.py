dca05542c671df685fc3c256d9c7c65d7d072d3a
Prashant <shishupal78008@gmail.com>
2026-05-16 19:03:33 +0530

    ddd

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -1,9 +1,5 @@
 
 
-from src.components.data_ingestion import DataIngestion
-from src.components.data_transformation import DataTransformation
-from src.components.model_trainer import ModelTrainer
-from src.components.model_evaluation import ModelEvaluation
 
 
 class TrainingPipeline:
@@ -12,11 +8,13 @@ class TrainingPipeline:
         pass
 
     def start_data_ingestion(self):
+        from src.components.data_ingestion import DataIngestion
         obj = DataIngestion()
         train_data_path, test_data_path = obj.initiate_data_ingestion()
         return train_data_path, test_data_path
 
     def start_data_transformation(self, train_data_path, test_data_path):
+        from src.components.data_transformation import DataTransformation
         data_transformation = DataTransformation()
         train_arr, test_arr = data_transformation.initialize_data_transformation(
             train_data_path,
@@ -25,10 +23,12 @@ class TrainingPipeline:
         return train_arr, test_arr
 
     def start_model_training(self, train_arr, test_arr):
+        from src.components.model_trainer import ModelTrainer
         model_trainer_obj = ModelTrainer()
         model_trainer_obj.initate_model_training(train_arr, test_arr)
 
     def start_model_evaluation(self, train_arr, test_arr):
+        from src.components.model_evaluation import ModelEvaluation
         model_eval_obj = ModelEvaluation()
         model_eval_obj.initiate_model_evaluation(train_arr, test_arr)
 
